0c58629f98b30fb3708abffce55e00ee5f62f82d
Paras Gupta <guptaparas039@gmail.com>
2021-10-01 00:33:07 +0530

    Create index.php
    Perform Linting and Indentation

--- index.php
+++ index.php
@@ -9,57 +9,56 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        
-<nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
-    <div class="container">
-        <a href="./" class="navbar-brand">
-            <i class="fas fa-blog"></i> &nbsp;
-            Foundation Bank
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="./" class="nav-link active">
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="./newreg.php" class="nav-link active">
-                        New Customers
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="./table.php" class="nav-link active">
-                        All Customers
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="./transfer.php" class="nav-link active">
-                        Transfer
-                    </a>
-                </li>
-            </ul>
+        <nav class="navbar navbar-dark navbar-expand-sm bg-dark fixed-top">
+            <div class="container">
+                <a href="./" class="navbar-brand">
+                    <i class="fas fa-blog"></i> &nbsp;
+                    Foundation Bank
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="navbarCollapse" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a href="./" class="nav-link active">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./newreg.php" class="nav-link active">
+                                New Customers
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./table.php" class="nav-link active">
+                                All Customers
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./transfer.php" class="nav-link active">
+                                Transfer
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="backg">
+            <div class="contain">
+                <div class="content">
+                    <i class="fas fa-5x fa-blog p-3"></i> 
+                    <h3>Welcome to</h3>
+                    <h1>Foundation Bank</h1>
+                    <a href="./newreg.php"><button type="button" class="btn btn-lg btn-primary">New Customer</button></a>
+                    <a href="./table.php"><button type="button" class="btn btn-lg btn-primary">All Customer</button></a>
+                    <a href="./transfer.php"><button type="button" class="btn btn-lg btn-primary">Transfer</button></a>
+                    <a href="https://rzp.io/l/cTUKoXgar8"><button type="button" class="btn btn-lg btn-primary">Donate</button></a>
+                </div>
+            </div>
         </div>
-    </div>
-</nav>
-<div class="backg">
-<div class="contain">
-    <div class="content">
-        <i class="fas fa-5x fa-blog p-3"></i> 
-        <h3>Welcome to</h3>
-        <h1>Foundation Bank</h1>
-        <a href="./newreg.php"><button type="button" class="btn btn-lg btn-primary">New Customer</button></a>
-        <a href="./table.php"><button type="button" class="btn btn-lg btn-primary">All Customer</button></a>
-        <a href="./transfer.php"><button type="button" class="btn btn-lg btn-primary">Transfer</button></a>
-        <a href="https://rzp.io/l/cTUKoXgar8"><button type="button" class="btn btn-lg btn-primary">Donate</button></a>
-    </div>
-</div>
-</div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </body>
 </html>
