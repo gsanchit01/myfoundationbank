63bd6609c90af8b96070c8a0ed81dfc7f6949619
gsanchit01 <70408013+gsanchit01@users.noreply.github.com>
2021-10-01 00:36:14 +0530

    Update open.php

--- open.php
+++ open.php
@@ -2,11 +2,8 @@
 	$server = "localhost";
     $username = "root";
     $password = "";
-
         $con = mysqli_connect($server, $username, $password,"banking");
-
     if(!$con){
         die("connection to this database failed due to" . mysqli_connect_error());
     }
 ?>
-
